8e4c347d496accad81e5363f7a5d82c60a490c4e
BlerrX <lyovghukasyan@gmail.com>
2020-05-07 00:47:04 +0400

    sepereting back to NodeBack branch

--- modeling/src/components/Mainpage/Container.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import MainPage from './Mainpage';
-import MobileMainPage from './MobileMainPage';
-const Container = _ => {
-  return _.matchMedia ? <MobileMainPage {..._} /> : <MainPage {..._} />;
-};
-export default Container;
