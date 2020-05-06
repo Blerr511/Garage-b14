a402ecf6f33a4638bce81ba9ea235e996f01b6ce
BlerrX <lyovghukasyan@gmail.com>
2020-05-07 00:44:35 +0400

    Sepereting app front to other branch

--- modeling back/helpers/decodeJWT.js
+++
@@ -1,7 +0,0 @@
-const jwt = require('jsonwebtoken');
-const CONFIG = require('../config.json');
-module.exports = async (req, res) => {
-  const token = req.cookies.token;
-  const data = jwt.verify(token, CONFIG.JWTsecret);
-  res.status(200).send(data);
-};
